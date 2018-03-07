54c400f4affd8315cfc490f9f737538b1fb17cb2
Alexandra Lehene <i.alesandra21@gmail.com>
2018-03-07 22:07:53 +0200

    - use a timer for long tap gesture

--- Chain Master/ViewController.h
+++ Chain Master/ViewController.h
@@ -1,14 +1,9 @@
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController<NSStreamDelegate, UIGestureRecognizerDelegate>
+@interface ViewController : UIViewController<NSStreamDelegate>
 
 @property (nonatomic, retain) NSInputStream *inputStream;
 @property (nonatomic, retain) NSOutputStream *outputStream;
-
-
-
-
-
 
 @end
 
--- Chain Master/ViewController.m
+++ Chain Master/ViewController.m
@@ -10,97 +10,81 @@
 @property (weak, nonatomic) IBOutlet UISegmentedControl *seg3;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *seg4;
 
-@property (nonatomic)  UIGestureRecognizerState gestureState;
+@property (nonatomic) NSTimer *timer;
 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    
-//    [self initNetworkCommunication];
+  [super viewDidLoad];
   
-    UILongPressGestureRecognizer *longPressOnButton = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPressOnButton:)];
-    longPressOnButton.delegate = self;
-    _goButton.userInteractionEnabled = YES;
-    [_goButton addGestureRecognizer:longPressOnButton];
+//  [self initNetworkCommunication];
 }
 
-- (void)longPressOnButton:(UILongPressGestureRecognizer*)gesture {
-  _gestureState = gesture.state;
-  [self start];
-
-}
-
-- (void)start {
-  while (_gestureState != UIGestureRecognizerStateEnded) {
-  NSLog(@"start %ld", (long)_gestureState);
-    //    [self sendMovementActionToController:_seg1];
-    //    [self sendMovementActionToController:_seg2];
-    //    [self sendMovementActionToController:_seg3];
-    //    [self sendMovementActionToController:_seg4];
- }
-  //  // When you stop touch the button
-  //  if (gesture.state == UIGestureRecognizerStateEnded) {
-  //    NSLog(@"stop");
-  //    [self writeOutputStreamWithTag:_seg1.tag command:@"STOP"];
-  //    [self writeOutputStreamWithTag:_seg2.tag command:@"STOP"];
-  //    [self writeOutputStreamWithTag:_seg3.tag command:@"STOP"];
-  //    [self writeOutputStreamWithTag:_seg4.tag command:@"STOP"];
-  //  }
-}
-
+#pragma mark connection
 - (void)initNetworkCommunication {
-    CFReadStreamRef readStream;
-    CFWriteStreamRef writeStream;
-    CFStreamCreatePairWithSocketToHost(NULL, (CFStringRef)@"192.168.0.199", 7777, &readStream, &writeStream);
-    _inputStream = (NSInputStream *)CFBridgingRelease(readStream);
-    _outputStream = (NSOutputStream *)CFBridgingRelease(writeStream);
-    
-    [_inputStream setDelegate:self];
-    [_outputStream setDelegate:self];
-    
-    [_inputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-    [_outputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-    
-    [_inputStream open];
-    [_outputStream open];
+  CFReadStreamRef readStream;
+  CFWriteStreamRef writeStream;
+  CFStreamCreatePairWithSocketToHost(NULL, (CFStringRef)@"192.168.0.199", 7777, &readStream, &writeStream);
+  _inputStream = (NSInputStream *)CFBridgingRelease(readStream);
+  _outputStream = (NSOutputStream *)CFBridgingRelease(writeStream);
+  
+  [_inputStream setDelegate:self];
+  [_outputStream setDelegate:self];
+  
+  [_inputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+  [_outputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+  
+  [_inputStream open];
+  [_outputStream open];
 }
 
+#pragma mark Touch interaction
+- (IBAction)theTouchDown:(id)sender {
+  NSLog(@"start touch");
+  _timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(startControllerActions) userInfo:nil repeats:true];
+}
+
+- (IBAction)theTouchUpInside:(id)sender {
+  NSLog(@"end touch");
+  [_timer invalidate];
+  _timer = nil;
+  
+  [self stopControllerActions];
+}
+
+- (void)startControllerActions {
+  [self sendMovementActionToController:_seg1];
+  [self sendMovementActionToController:_seg2];
+  [self sendMovementActionToController:_seg3];
+  [self sendMovementActionToController:_seg4];
+}
+
+- (void)stopControllerActions {
+  [self writeOutputStreamWithTag:_seg1.tag command:@"STOP"];
+  [self writeOutputStreamWithTag:_seg2.tag command:@"STOP"];
+  [self writeOutputStreamWithTag:_seg3.tag command:@"STOP"];
+  [self writeOutputStreamWithTag:_seg4.tag command:@"STOP"];
+}
+
+#pragma mark formatting methods
 - (void)sendMovementActionToController:(UISegmentedControl *) segmentedControl {
-    UISegmentedControl *button = ((UISegmentedControl*)segmentedControl);
-    long tag = button.tag;
-    
-    if(button.selectedSegmentIndex == 0) {
-        [self writeOutputStreamWithTag:tag command:@"UP"];
-    } else if(button.selectedSegmentIndex == 2) {
-        [self writeOutputStreamWithTag:tag command:@"DOWN"];
-    }
+  UISegmentedControl *button = ((UISegmentedControl*)segmentedControl);
+  long tag = button.tag;
+//  NSLog(@"hshshhs %ld tag-> %ld", button.selectedSegmentIndex, tag );
+  if(button.selectedSegmentIndex == 0) {
+    [self writeOutputStreamWithTag:tag command:@"UP"];
+  } else if(button.selectedSegmentIndex == 2) {
+    [self writeOutputStreamWithTag:tag command:@"DOWN"];
+  }
 }
 
 - (void)writeOutputStreamWithTag:(long)tag command:(NSString *)command {
-    NSString *response  = [NSString stringWithFormat:@"Motor%ld%@", tag , command];
-    NSData *data = [[NSData alloc] initWithData:[response dataUsingEncoding:NSASCIIStringEncoding]];
-    [_outputStream write:[data bytes] maxLength:[data length]];
-    NSLog(@"%@", response);
+  NSString *response  = [NSString stringWithFormat:@"Motor%ld%@", tag , command];
+  NSData *data = [[NSData alloc] initWithData:[response dataUsingEncoding:NSASCIIStringEncoding]];
+  [_outputStream write:[data bytes] maxLength:[data length]];
+  NSLog(@"%@", response);
 }
-//
-//- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)eventt {
-//    // TODO we should collect all data from seg controller when the touch was initiated
-//}
-//
-- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    UITouch *touch = [[event allTouches] anyObject];
-    CGPoint touchLocation = [touch locationInView:self.view];
-
-    if (CGRectContainsPoint(_goButton.frame, touchLocation)) {
-        //this should be called continously while keeping go button pressed
-        NSLog(@" touched");
-    }
-}
-//
-//- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-//}
 
 @end
