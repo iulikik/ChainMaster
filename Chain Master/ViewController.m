7c25b2d534b4588a2569916010c9e5fbff4f5693
Roberto <vlad.drahnea@gmail.com>
2018-03-07 12:35:54 +0200

    now go button should work properly ;)

--- Chain Master/ViewController.m
+++ Chain Master/ViewController.m
@@ -1,27 +1,109 @@
-#import <Foundation/Foundation.h>
+//
+//  ViewController.m
+//  Chain Master
+//
+//  Created by IULIAN MORARI on 1/31/18.
+//  Copyright © 2018 Ik Moraru. All rights reserved.
+//
+
 #import "ViewController.h"
 
+typedef NS_ENUM(NSUInteger, CMMotorCommand) {
+    CMMotorCommandUp = 0,
+    CMMotorCommandStop = 1,
+    CMMotorCommandDown = 2
+};
+
+typedef NS_ENUM(NSUInteger, CMMotorID) {
+    CMMotorID1 = 1,
+    CMMotorID2 = 2,
+    CMMotorID3 = 3,
+    CMMotorID4 = 4
+};
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *goButton;
-@property (weak, nonatomic) IBOutlet UISegmentedControl *seg1;
-@property (weak, nonatomic) IBOutlet UISegmentedControl *seg2;
-@property (weak, nonatomic) IBOutlet UISegmentedControl *seg3;
-@property (weak, nonatomic) IBOutlet UISegmentedControl *seg4;
+@property (strong, atomic) NSMutableDictionary *commands;
+
 @end
 
 @implementation ViewController
+
++ (NSString*) stringForCommand:(CMMotorCommand)command {
+    NSString *string = @"STOP";
+    
+    switch (command) {
+        case CMMotorCommandStop:
+            string = @"STOP";
+            break;
+            
+        case CMMotorCommandUp:
+            string = @"UP";
+            break;
+            
+        case CMMotorCommandDown:
+            string = @"DOWN";
+            break;
+            
+        default:
+            break;
+    }
+    
+    return string;
+}
+
++ (NSString*) stringForMotorID:(CMMotorID)motorID {
+    NSString *string = @"0";
+    
+    switch (motorID) {
+        case CMMotorID1:
+            string = @"Motor1";
+            break;
+            
+        case CMMotorID2:
+            string = @"Motor2";
+            break;
+            
+        case CMMotorID3:
+            string = @"Motor3";
+            break;
+            
+        case CMMotorID4:
+            string = @"Motor4";
+            break;
+            
+        default:
+            break;
+    }
+    
+    return string;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     [self initNetworkCommunication];
+    
+    NSString* stopCommand = [ViewController stringForCommand:CMMotorCommandStop];
+    
+    self.commands = [NSMutableDictionary dictionaryWithDictionary:@{
+                                                                    [ViewController stringForMotorID:CMMotorID1]: stopCommand,
+                                                                    [ViewController stringForMotorID:CMMotorID1]: stopCommand,
+                                                                    [ViewController stringForMotorID:CMMotorID1]: stopCommand,
+                                                                    [ViewController stringForMotorID:CMMotorID1]: stopCommand,
+                                                                    }];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 - (void)initNetworkCommunication {
     CFReadStreamRef readStream;
     CFWriteStreamRef writeStream;
-    CFStreamCreatePairWithSocketToHost(NULL, (CFStringRef)@"192.168.0.199", 7777, &readStream, &writeStream);
+    CFStreamCreatePairWithSocketToHost(NULL, (CFStringRef)@"192.168.0.39", 7777, &readStream, &writeStream);
     _inputStream = (NSInputStream *)CFBridgingRelease(readStream);
     _outputStream = (NSOutputStream *)CFBridgingRelease(writeStream);
     
@@ -35,47 +117,37 @@
     [_outputStream open];
 }
 
-- (void)sendMovementActionToController:(UISegmentedControl *) segmentedControl {
-    UISegmentedControl *button = ((UISegmentedControl*)segmentedControl);
-    long tag = button.tag;
+- (IBAction)ToggleLight:(id)sender {
     
-    if(button.selectedSegmentIndex == 0) {
-        [self writeOutputStreamWithTag:tag command:@"UP"];
-    } else if(button.selectedSegmentIndex == 2) {
-        [self writeOutputStreamWithTag:tag command:@"DOWN"];
-    }
+    UISegmentedControl *button = ((UISegmentedControl*)sender);
+    CMMotorID motorID = button.tag;
+    CMMotorCommand command = button.selectedSegmentIndex;
+    NSString* commandString = [ViewController stringForCommand:command];
+    NSString* motorIDString = [ViewController stringForMotorID:motorID];
+    
+    self.commands[motorIDString] = commandString;
 }
 
-- (void)writeOutputStreamWithTag:(long)tag command:(NSString *)command {
-    NSString *response  = [NSString stringWithFormat:@"Motor%ld%@", tag , command];
+- (void)sendCommand:(NSString*)command toMotor:(NSString*)motorID {
+    NSString *response  = [NSString stringWithFormat:@"%@%@", motorID , command];
+    
     NSData *data = [[NSData alloc] initWithData:[response dataUsingEncoding:NSASCIIStringEncoding]];
     [_outputStream write:[data bytes] maxLength:[data length]];
+    
     NSLog(@"%@", response);
 }
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)eventt {
-    // TODO we should collect all data from seg controller when the touch was initiated
-}
-
-- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    UITouch *touch = [[event allTouches] anyObject];
-    CGPoint touchLocation = [touch locationInView:self.view];
+- (IBAction)goButtonPressed:(id)sender {
+    NSLog(@"Sending command to control unit...");
     
-    if (CGRectContainsPoint(_goButton.frame, touchLocation)) {
-        //this should be called continously while keeping go button pressed
-        NSLog(@" touched");
-        [self sendMovementActionToController:_seg1];
-        [self sendMovementActionToController:_seg2];
-        [self sendMovementActionToController:_seg3];
-        [self sendMovementActionToController:_seg4];
-    }
-}
-
-- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    [self writeOutputStreamWithTag:_seg1.tag command:@"STOP"];
-    [self writeOutputStreamWithTag:_seg2.tag command:@"STOP"];
-    [self writeOutputStreamWithTag:_seg3.tag command:@"STOP"];
-    [self writeOutputStreamWithTag:_seg4.tag command:@"STOP"];
+    for (NSString *key in _commands) {
+        NSString *commandValue = _commands[key];
+        
+        [self sendCommand:commandValue toMotor:key];
+    };
+    
+    NSLog(@"Commands were sent!");
 }
 
 @end
+
